1ee64c4fdbeb64e41f0d261dcee04a40d99a0df9
Hunterl84 <hunterl0825@gmail.com>
2026-03-28 01:05:38 -0500

    Monte-Carlo Updates
    Adjusting monte carlo simulation.

--- portfolio/Monte-Carlo.py
+++ portfolio/Monte-Carlo.py
@@ -1,37 +1,49 @@
 ## Monte Carlo portfolio simulation
 import pandas as pd
-import numpy as np
+# import numpy as np
 import random
-
-store = {"Value", "Company"}
+import plotly.express as pe
 
 def monteCarlo(file : str, numSims : int, risk : float):
 
     finalRisk = risk / 100
     argFile = pd.read_csv(file)
     df = pd.DataFrame(argFile)
+    quickDF = pd.DataFrame(columns=("Company", "Value"))
 
     print("Initial Investments")
     print(df)
+    print("Temp: ", quickDF)
 
     valueSum = df['Value'].sum()
     compSum = df['Company'].count()
 
+    #   This is the start of the simulation 
+    print("Starting Monte Carlo Simulation")
+    print("Total Number of Simulations: ", numSims)
     print("Total Number of Companyies: ", compSum)  # find the final number of companies
     print("Total Value: $", valueSum)
 
     start = 0
     while start < numSims:
+
         start += 1
         print("Sim Number: ", start)
+
         for x, y in df.itertuples(index=False):
-            print("Company: ", x)
-            print("Original Number:", y)
+
             change = y * finalRisk
             finalValue = random.uniform(a=(y + change), b=(y - change))
-            print("Final Value: ", finalValue)
+            quickDF.loc[len(quickDF)] = [x, finalValue]
 
-    print(store)
+            # print("Company: ", x) # Debugging
+            # print("Original Number:", y)  # Debugging
+            # print("Final Value: ", finalValue)    #Debugging
+
+    print("Checking Values: \n", quickDF)
+
+    fig = pe.histogram(y=("Company"), x="Value")
+    fig.show()
 
             
 
